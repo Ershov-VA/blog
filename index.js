68494a10cc007493c7150b4e67bf27ad90de30ca
Vladimir <ershov_va@bk.ru>
2019-02-08 16:55:47 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", (req, res) => res.send('hello world'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const arr = ['hello', 'world', 'test'];
+
+app.get("/", (req, res) => res.render('index', {
+    arr: arr
+}));
+
+
+app.get("/create", (req, res) => res.render('create'));
+app.post("/create", (req, res) => {
+    arr.push(req.body.title);
+    res.render('index', {arr: arr});
+});
 
 app.listen(3000, () => console.log('app listening 3000'));
