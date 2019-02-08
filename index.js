22035b041de3ca819ab4b0b9a2b69f901446e911
Vladimir <ershov_va@bk.ru>
2019-02-08 14:58:44 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const test = null;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send('hello world'));
+
+app.listen(3000, () => console.log('app listening 3000'));
